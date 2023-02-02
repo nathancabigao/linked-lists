2075a18bfd869d55977b815d7d8abf2b83ef372e
nathancabigao <nathan0714@hotmail.com>
2023-02-02 15:21:48 -0500

    Initialize Node class

---
+++ main.rb
@@ -0,0 +1,15 @@
+# frozen-string-literal: true
+
+# Class for creating instances of LinkedLists
+class LinkedList
+end
+
+# Class for creating Nodes to be used in LinkedList
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
