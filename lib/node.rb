ab3e06eea4d167fa31ae90877170ac6db4f179b2
nathancabigao <nathan0714@hotmail.com>
2023-02-02 16:25:44 -0500

    Move classes into files in lib

---
+++ lib/node.rb
@@ -0,0 +1,11 @@
+# frozen-string-literal: true
+
+# Class for creating Nodes to be used in LinkedList
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
